c8826d687938882836685f7bba94f1099f5cbf43
palec87 <david@stanka.de>
2025-03-02 22:01:01 +0000

    fix attempt

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,24 @@
+// import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { mdsvex } from 'mdsvex';
+// import mdsvexConfig from './mdsvex.config.js';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	extensions: ['.svelte', ...mdsvexConfig.extensions],
+// 	kit: {
+// 		adapter: adapter(),
+
+// 		// remove this if you're not using comment system
+// 		csp: { mode: 'auto' }
+// 	},
+// 	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
+// };
+
+// export default config;
+
+
+
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
@@ -8,11 +29,36 @@ const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
 		adapter: adapter(),
-
-		// remove this if you're not using comment system
-		csp: { mode: 'auto' }
+		csp: { mode: 'auto' },
+		alias: {
+			$components: 'src/lib/components',
+			$lib: 'src/lib',
+			$routes: 'src/routes'
+		}
 	},
 	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
 };
 
 export default config;
+
+
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
+// import mdsvexConfig from './mdsvex.config.js';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   extensions: ['.svelte', ...mdsvexConfig.extensions],
+//   preprocess: preprocess(),
+
+//   kit: {
+//     adapter: adapter(),
+//     alias: {
+//       $components: 'src/lib/components',
+//       $lib: 'src/lib',
+//       $routes: 'src/routes'
+//     }
+//   }
+// };
+
+// export default config;
